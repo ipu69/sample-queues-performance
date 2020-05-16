5cb003034662a9b1d64826e88d9fae9be2435231
Andrey Larionov <larioandr@gmail.com>
2020-05-16 16:46:33 +0300

    add sample python code with dockerfile

---
+++ mm1_python/model.py
@@ -0,0 +1,23 @@
+# M/M/1 tandem network simulation.
+# import argparse
+import sys
+
+# def simulate():
+#     pass
+
+
+# def parse_arguments():
+#     parser = argparse.ArgumentParser(
+#         description="Simulate M/M/1 -> ... */M/1 tandem network.")
+#     parser.add_argument('')
+def x_plus_y(x, y):
+    return x + y
+
+
+if __name__ == '__main__':
+    print('Hello, dockerized python app!')
+    print('Your args are: ', sys.argv)
+    if len(sys.argv) > 1:
+        with open(sys.argv[-1]) as f:
+            for line in f:
+                print(line)
---
+++ mm1_python/model_test.py
@@ -0,0 +1,5 @@
+from model import x_plus_y
+
+
+def test_x_plus_y():
+    assert x_plus_y(10, 3) == 13
