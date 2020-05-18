8161e0658f4d99ba9a5bf3f647522bd7a882ead2
Andrey Larionov <larioandr@gmail.com>
2020-05-18 19:51:22 +0300

    rename workflow, add linting errors

--- queue_python/model_test.py
+++ queue_python/model_test.py
@@ -2,5 +2,5 @@ from model import x_plus_y
 
 
 def test_x_plus_y():
-    assert x_plus_y(10, 3) == 13
-    assert x_plus_y('a', 'b') == 'ab'
+    assert x_plus_y(10, 3)== 13
+    assert x_plus_y('a', 'b') =='ab'
